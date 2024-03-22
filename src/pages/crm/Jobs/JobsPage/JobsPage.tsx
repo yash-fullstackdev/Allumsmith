c7f1ebc4f13fdab45fb8037a77733658bf312061
Yuvrajsinh2111 <yuvrajsinh.borasiya@bitontree.com>
2024-03-22 16:56:51 +0530

    fix dd issue for color while adding job

--- src/pages/crm/Jobs/JobsPage/JobsPage.tsx
+++ src/pages/crm/Jobs/JobsPage/JobsPage.tsx
@@ -23,9 +23,10 @@ const JobsPage = () => {
     const [branchId, setBranchId] = useState('');
     const navigate = useNavigate();
     const [productTransfer, setProductTransfer] = useState(false)
-    const [product,setProduct] = useState('');
-    const [length,setLength] = useState('');
-    
+    const [product, setProduct] = useState('');
+    const [length, setLength] = useState('');
+    const [colorDataList, setColorDataList] = useState<Array<any>>([]);
+
 
     const getProductDetails = async () => {
         try {
@@ -63,7 +64,7 @@ const JobsPage = () => {
     }, []);
 
     const handleAddEntry = () => {
-        setEntries([...entries, { product: '', quantity: '', coating: '', color: '',length: '' }]);
+        setEntries([...entries, { product: '', quantity: '', coating: '', color: '', length: '' }]);
     };
 
     const handleSaveEntries = async () => {
@@ -96,14 +97,26 @@ const JobsPage = () => {
         setEntries(newProduct)
     }
 
-    const updateColorOptions = (coatingId: any) => {
+    // const updateColorOptions = (coatingId: any) => {
+    //     const selectedCoating = coatingData.find((coating: any) => coating._id === coatingId);
+    //     if (selectedCoating) {
+    //         setColorData(selectedCoating.colors);
+    //     } else {
+    //         setColorData([]);
+    //     }
+    // }
+    const updateColorOptions = (coatingId: any, entryIndex: number) => {
         const selectedCoating = coatingData.find((coating: any) => coating._id === coatingId);
         if (selectedCoating) {
-            setColorData(selectedCoating.colors);
+            const newColorDataList = [...colorDataList];
+            newColorDataList[entryIndex] = selectedCoating.colors;
+            setColorDataList(newColorDataList);
         } else {
-            setColorData([]);
+            const newColorDataList = [...colorDataList];
+            newColorDataList[entryIndex] = [];
+            setColorDataList(newColorDataList);
         }
-    }
+    };
 
     return (
         <PageWrapper name='ADD PRODUCTS' isProtectedRoute={true}>
@@ -210,88 +223,88 @@ const JobsPage = () => {
                                                                 )}
                                                             </div>
                                                             <div key={index} className='mt-2 grid grid-cols-12 gap-1'>
-                                                            {!productTransfer ? (<>
-                                                                <div className='col-span-12 lg:col-span-3'>
-                                                                    <Label htmlFor={`name-${index}`}>
-                                                                        Products
-                                                                        <span className='ml-1 text-red-500'>*</span>
-                                                                    </Label>
-                                                                    <Select
-                                                                        placeholder='Select Product'
-                                                                        id={`product`}
-                                                                        name={`product`}
-                                                                        value={entry.product}
-                                                                        onChange={(e) => {
-                                                                            const updatedEntries = [...entries];
-                                                                            updatedEntries[index].product = e.target.value;
-                                                                            setEntries(updatedEntries);
-                                                                        }}
-                                                                    >
-                                                                        {productsData.map((item: any) => (
-                                                                            <option key={item._id} value={item._id}>
-                                                                                {item.name}
-                                                                            </option>
-                                                                        ))}
-                                                                    </Select>
+                                                                {!productTransfer ? (<>
+                                                                    <div className='col-span-12 lg:col-span-3'>
+                                                                        <Label htmlFor={`name-${index}`}>
+                                                                            Products
+                                                                            <span className='ml-1 text-red-500'>*</span>
+                                                                        </Label>
+                                                                        <Select
+                                                                            placeholder='Select Product'
+                                                                            id={`product`}
+                                                                            name={`product`}
+                                                                            value={entry.product}
+                                                                            onChange={(e) => {
+                                                                                const updatedEntries = [...entries];
+                                                                                updatedEntries[index].product = e.target.value;
+                                                                                setEntries(updatedEntries);
+                                                                            }}
+                                                                        >
+                                                                            {productsData.map((item: any) => (
+                                                                                <option key={item._id} value={item._id}>
+                                                                                    {item.name}
+                                                                                </option>
+                                                                            ))}
+                                                                        </Select>
 
-                                                                </div>
-                                                                {entry.product && (
-    <div className='col-span-12 lg:col-span-3'>
-        {/* Add your additional input fields here */}
-        {/* For example, you can add an input field for quantity */}
-        <Label htmlFor={`length-${index}`}>
-            Length 
-            <span className='ml-1 text-red-500'>*</span>
-        </Label>
-        <Input
-            type='number'
-            id={`length-${index}`}
-            name={`length-${index}`}
-            value={productsData.find((item: any) => item._id === entry.product)?.length || ''}
-        />
-    </div>
-)}
-                                                            </>
-                                                               ) : (<>
-                                                               <div className='col-span-12 lg:col-span-3'>
-                                        <Label htmlFor={`product`}>
-                                            Product
-                                            <span className='ml-1 text-red-500'>*</span>
-                                        </Label>
-                                        <Input
-                                            type='text'
-                                            id={`product`}
-                                            name={`product`}
-                                            value={entry.product}
-                                                                        onChange={(e) => {
-                                                                            const updatedEntries = [...entries];
-                                                                            updatedEntries[index].product = e.target.value;
-                                                                            setEntries(updatedEntries);
-                                                                        }}
-                                        />
-                                        {/* ... Error handling for hsn field */}
-                                    </div>
-                                    <div className='col-span-12 lg:col-span-3'>
-                                        <Label htmlFor={`length`}>
-                                            Length
-                                            <span className='ml-1 text-red-500'>*</span>
-                                        </Label>
-                                        <Input
-                                            type='number'
-                                            id={`length`}
-                                            name={`length`}
-                                            value={entry.length}
-                                            onChange={(e) => {
-                                                const updatedEntries = [...entries];
-                                                updatedEntries[index].length = e.target.value;
-                                                setEntries(updatedEntries);
-                                                                                                            }}
-                                        />
-                                        {/* ... Error handling for hsn field */}
-                                    </div>
+                                                                    </div>
+                                                                    {entry.product && (
+                                                                        <div className='col-span-12 lg:col-span-3'>
+                                                                            {/* Add your additional input fields here */}
+                                                                            {/* For example, you can add an input field for quantity */}
+                                                                            <Label htmlFor={`length-${index}`}>
+                                                                                Length
+                                                                                <span className='ml-1 text-red-500'>*</span>
+                                                                            </Label>
+                                                                            <Input
+                                                                                type='number'
+                                                                                id={`length-${index}`}
+                                                                                name={`length-${index}`}
+                                                                                value={productsData.find((item: any) => item._id === entry.product)?.length || ''}
+                                                                            />
+                                                                        </div>
+                                                                    )}
+                                                                </>
+                                                                ) : (<>
+                                                                    <div className='col-span-12 lg:col-span-3'>
+                                                                        <Label htmlFor={`product`}>
+                                                                            Product
+                                                                            <span className='ml-1 text-red-500'>*</span>
+                                                                        </Label>
+                                                                        <Input
+                                                                            type='text'
+                                                                            id={`product`}
+                                                                            name={`product`}
+                                                                            value={entry.product}
+                                                                            onChange={(e) => {
+                                                                                const updatedEntries = [...entries];
+                                                                                updatedEntries[index].product = e.target.value;
+                                                                                setEntries(updatedEntries);
+                                                                            }}
+                                                                        />
+                                                                        {/* ... Error handling for hsn field */}
+                                                                    </div>
+                                                                    <div className='col-span-12 lg:col-span-3'>
+                                                                        <Label htmlFor={`length`}>
+                                                                            Length
+                                                                            <span className='ml-1 text-red-500'>*</span>
+                                                                        </Label>
+                                                                        <Input
+                                                                            type='number'
+                                                                            id={`length`}
+                                                                            name={`length`}
+                                                                            value={entry.length}
+                                                                            onChange={(e) => {
+                                                                                const updatedEntries = [...entries];
+                                                                                updatedEntries[index].length = e.target.value;
+                                                                                setEntries(updatedEntries);
+                                                                            }}
+                                                                        />
+                                                                        {/* ... Error handling for hsn field */}
+                                                                    </div>
 
-                                                               
-                                                           </>) }
+
+                                                                </>)}
                                                                 <div className='col-span-12 lg:col-span-3'>
                                                                     <Label htmlFor={`hsn-${index}`}>
                                                                         Quantity
@@ -318,13 +331,13 @@ const JobsPage = () => {
                                                                         placeholder='Select Coating'
                                                                         id={`coating-${index}`}
                                                                         name={`coating-${index}`}
-                                                                        value={entry.coating._id} // Use coating _id as the value
+                                                                        value={entry.coating}
                                                                         onChange={(e: any) => {
                                                                             const coatingId = e.target.value;
                                                                             const updatedEntries = [...entries];
-                                                                            updatedEntries[index].coating = e.target.value;
+                                                                            updatedEntries[index].coating = coatingId;
                                                                             setEntries(updatedEntries);
-                                                                            updateColorOptions(coatingId);
+                                                                            updateColorOptions(coatingId, index);
                                                                         }}
                                                                     >
                                                                         {coatingData.map((coating: any) => (
@@ -344,14 +357,15 @@ const JobsPage = () => {
                                                                             placeholder='Select Color'
                                                                             id={`color-${index}`}
                                                                             name={`color-${index}`}
-                                                                            value={entry.color} // Use color value from state
+                                                                            value={entry.color}
                                                                             onChange={(e: any) => {
                                                                                 const updatedEntries = [...entries];
                                                                                 updatedEntries[index].color = e.target.value;
                                                                                 setEntries(updatedEntries);
                                                                             }}
                                                                         >
-                                                                            {colorData.map((color: any) => (
+                                                                            {/* Use color data specific to the entry */}
+                                                                            {colorDataList[index]?.map((color: any) => (
                                                                                 <option key={color._id} value={color._id}>
                                                                                     {color.name}
                                                                                 </option>
