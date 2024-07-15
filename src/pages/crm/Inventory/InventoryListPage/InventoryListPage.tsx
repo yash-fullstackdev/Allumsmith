634a1e93c08f5f176cd433b5f9387cf0e92f5fec
Yuvrajsinh2111 <yuvrajsinh.borasiya@bitontree.com>
2024-07-15 16:09:53 +0530

    fix: hide stock action

--- src/pages/crm/Inventory/InventoryListPage/InventoryListPage.tsx
+++ src/pages/crm/Inventory/InventoryListPage/InventoryListPage.tsx
@@ -202,11 +202,11 @@ const InventoryListPage = () => {
                         <CardHeaderChild>
                             <CardTitle><h1>All Inventory</h1></CardTitle>
                         </CardHeaderChild>
-                        <PermissionGuard permissionType="write">
+                        {/* <PermissionGuard permissionType="write">
                         <Button variant='solid' icon='HeroPlus' onClick={() => setStockActionModal(true)}>
                             Stock Action
                         </Button>
-                        </PermissionGuard>
+                        </PermissionGuard> */}
 
                     </CardHeader>
                     <CardBody>
